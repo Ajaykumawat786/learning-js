be2c65b8aa7aee64107aa0246ce6ffff6c649ce5
Your GitHub Username <your-ajaykumawat2107@example.com>
2025-04-28 11:17:40 +0530

    Initial commit - learning js upload

---
+++ condition-in-js/js/script.js
@@ -0,0 +1,64 @@
+// if else statements
+
+// syntex
+//(intial, condition, inc/dec)
+
+//   {    education code     }
+
+
+let name = "shyam"                  // no
+let age = 15
+
+if( age >= 18  ){
+ 
+    console.log(name, "yes, you are eligible for applying driving licence")
+} else {
+    console.log(name, "No, you are not eligible for applying driving licence")
+}
+
+
+// And opertor 
+
+let name2 = "Ram"           
+let age2 = 55
+
+if( age2 >= 18 && age2 <= 60 ){                         // yes
+ 
+    console.log(name2, "yes, you are eligible for applying driving licence")
+} else {
+    console.log(name2, "No, you are not eligible for applying driving licence")
+}
+
+
+// week 
+
+let today = "monday"        // if we chand the command we got diffe]rent result
+
+if(today == "sunday") {
+    console.log( today, "Sunday is Funny day ")
+    alert("today we will going to waterpark")
+} 
+else if ( today == "monday") {
+    console.log(today, "the first working day in week")
+    
+}
+ else if ( today == "thuesday") {
+    console.log(today, " jai shree ram we will go to temple")
+} 
+else if ( today == "wednesday") {
+    console.log(today, "today i'm going to date")
+    
+}
+else if ( today == "Tuesday") {
+    console.log(today, "today om santi ")
+
+} 
+else if ( today == "friday") {
+ console.log(today,  "last working day in this week ")
+    
+} else{
+    "today going to goa"
+}
+    
+
+
